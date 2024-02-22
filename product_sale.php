0e9fdc6ba69b6f825b4f399a53f0f2be0c97b6ca
K-A-Tushar <katushar.isdb57@gmail.com>
2024-02-22 14:21:29 +0600

    working..

---
+++ product_sale.php
@@ -0,0 +1,11 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="canonical" href="https://getbootstrap.com/docs/5.3/getting-started/introduction/">
+<div class="container-xxl bd-gutter mt-3 my-md-4 bd-layout">
+    <aside class="bd-sidebar">side</aside>
+    <main class="bd-main order-1">
+        <div class="bd-intro pt-2 ps-lg-2">01</div>
+        <div class="bd-toc mt-3 mb-5 my-lg-0 mb-lg-5 px-sm-1 text-body-secondary">02</div>
+        <div class="bd-content ps-lg-2">03</div>
+    </main>
+</div>
