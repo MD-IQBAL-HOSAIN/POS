80375a1da27911fbff816b4dc6e77fe71645bb9c
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-02-24 23:16:59 +0600

    pos

--- admin/components/sidebar.php
+++ admin/components/sidebar.php
@@ -56,7 +56,7 @@
                         
                     </nav>
                 </div> -->
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                <!-- <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                     Pages
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
@@ -86,7 +86,7 @@
                             </nav>
                         </div>
                     </nav>
-                </div>
+                </div> -->
                 <!-- <div class="sb-sidenav-menu-heading">Addons</div>
                 <a class="nav-link" href="charts.html">
                     <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
