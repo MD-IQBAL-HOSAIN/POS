13c866908cdb806261818601dad97dc757fe6614
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-02-27 23:29:13 +0600

    Project pos

--- components/menubar.php
+++ components/menubar.php
@@ -3,10 +3,10 @@
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == 'true') {
 ?>
 <div class="row d-flex">
-    <div class="col-11">
+    <div class="col-10">
     <h1 class="text-center font-monospace" style="text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">User Management</h1>
     </div>
-    <div class="col-1">
+    <div class="col-2">
     <button class="btn btn-outline-warning"><a style="text-decoration: none;" href="logout.php">Log Out</a></button>
     </div>
 </div>
